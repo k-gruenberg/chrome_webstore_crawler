1e112428c51d6adf792c7e541b6860036fdbb166
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-07-16 02:52:11 +0200

    added some more ideas on statistics one could create

--- chrome_webstore_crawler.py
+++ chrome_webstore_crawler.py
@@ -39,7 +39,7 @@ print(f"Content of sitemap.xml reads: {sitemap_xml_content[:10]} ... {sitemap_xm
 #   => For each extension listed there:
 #      => Visit the extension's URL and parse title, description, no. of users, no. of ratings, average rating, version no., size, last updated and no. of languages.
 #      => Collect all of this information in a single 'extensions.csv' file, together with each extension's ID. Ensure (using the ID) that every extension is listed at most once!
-#      => If the --download flag is set, try to download each extension as a .crx file as well.
+#      => If the --download flag is set, try to download each extension as a .crx file as well. Should this fail, don't abort but simply skip.
 # Skip this step if a file named 'extensions.csv' already exists.
 # ##### ##### ##### #### ##### ##### ##### ##### ####
 
@@ -51,10 +51,12 @@ if not extensions_csv_file.is_file():
 
 # ##### ##### ##### ##### Step 3: ##### ##### ##### ####
 # Compute some statistics based on 'extensions.csv':
-# (1.) plot cumulative distribution function of extension size in KB
-# (2.) plot cumulative distribution function of time since last update in months
-# (3.) plot cumulative distribution function of no. of users
-# (4.) plot correlation between no. of users and time since last update in months (scatter plot)
+# (1.) plot cumulative distribution function of extension size in KB => important as larger extensions are generally harder to analyze
+# (2.) plot cumulative distribution function of time since last update in months => might show that there are many abandoned extensions out there
+# (3.) plot cumulative distribution function of no. of users => might show that most extensions in the store are rarely downloaded
+# (4.) plot cumulative distribution function of no. of users as percentage of sum of *all* users => might show that there are many users of small extensions
+# (5.) plot correlation between no. of users and time since last update in months (scatter plot) => how frequently are abandoned extensions used by users?
+# (6.) plot correlation between no. of users and extension size => do users generally use more complex (and therefore harder to analyze) extensions?
 # ##### ##### ##### #### ##### ##### ##### ##### ####
 
 # ToDo...

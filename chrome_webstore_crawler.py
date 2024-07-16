d8312d927284af8864362fd314fe10c8280769dd
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-07-16 02:43:48 +0200

    outlined todos for steps 2 and 3

--- chrome_webstore_crawler.py
+++ chrome_webstore_crawler.py
@@ -31,3 +31,32 @@ with open(sitemap_xml_file, 'r') as f:
 print("sitemap.xml has been read in.")
 
 print(f"Content of sitemap.xml reads: {sitemap_xml_content[:10]} ... {sitemap_xml_content[-10:]}")
+
+
+
+# ##### ##### ##### ##### Step 2: ##### ##### ##### ####
+# Visit each URL listed in 'sitemap.xml'.
+#   => For each extension listed there:
+#      => Visit the extension's URL and parse title, description, no. of users, no. of ratings, average rating, version no., size, last updated and no. of languages.
+#      => Collect all of this information in a single 'extensions.csv' file, together with each extension's ID. Ensure (using the ID) that every extension is listed at most once!
+#      => If the --download flag is set, try to download each extension as a .crx file as well.
+# Skip this step if a file named 'extensions.csv' already exists.
+# ##### ##### ##### #### ##### ##### ##### ##### ####
+
+extensions_csv_file = Path("extensions.csv")
+if not extensions_csv_file.is_file():
+	pass # ToDo...
+
+
+
+# ##### ##### ##### ##### Step 3: ##### ##### ##### ####
+# Compute some statistics based on 'extensions.csv':
+# (1.) plot cumulative distribution function of extension size in KB
+# (2.) plot cumulative distribution function of time since last update in months
+# (3.) plot cumulative distribution function of no. of users
+# (4.) plot correlation between no. of users and time since last update in months (scatter plot)
+# ##### ##### ##### #### ##### ##### ##### ##### ####
+
+# ToDo...
+
+
